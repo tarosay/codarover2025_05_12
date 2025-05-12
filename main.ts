d3639b5bde755419538a629fdb34da7f9e7f99d0
Tarosa <tarosa.y@gmail.com>
2025-05-13 03:30:11 +0900

    回転テスト

--- main.ts
+++ main.ts
@@ -1,10 +1,7 @@
 let power = 0
 function car (テキスト: string, 数値: number) {
     if (テキスト == "前") {
-        pins.digitalWritePin(DigitalPin.P8, 0)
-        pins.digitalWritePin(DigitalPin.P9, 1)
-        pins.digitalWritePin(DigitalPin.P14, 0)
-        pins.digitalWritePin(DigitalPin.P15, 1)
+    	
     } else if (テキスト == "後") {
         pins.digitalWritePin(DigitalPin.P8, 1)
         pins.digitalWritePin(DigitalPin.P9, 0)
@@ -14,9 +11,26 @@ function car (テキスト: string, 数値: number) {
     	
     }
     power = 1024 * (数値 / 100)
-    pins.analogWritePin(AnalogPin.P13, power)
-    pins.analogWritePin(AnalogPin.P16, power)
 }
 basic.forever(function () {
-    car("前", 50)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P9, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.analogWritePin(AnalogPin.P13, 512)
+    pins.analogWritePin(AnalogPin.P16, 512)
+    basic.pause(2000)
+    pins.analogWritePin(AnalogPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P16, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.analogWritePin(AnalogPin.P13, 512)
+    pins.analogWritePin(AnalogPin.P16, 512)
+    basic.pause(2000)
+    pins.analogWritePin(AnalogPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P16, 0)
+    basic.pause(1000)
 })
