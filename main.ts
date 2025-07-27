3f2b18652b49d49e1a2b4cb7f182ef3fba7d472a
Tarosa <tarosa.y@gmail.com>
2025-07-28 01:31:48 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 let 動く右 = 0
 let 動く左 = 0
+let P13_PWM = 0
+let P16_PWM = 0
 function 動く (右タイヤ: number, 左タイヤ: number) {
     if (右タイヤ >= 0) {
         pins.digitalWritePin(DigitalPin.P8, 0)
@@ -19,8 +21,16 @@ function 動く (右タイヤ: number, 左タイヤ: number) {
         pins.digitalWritePin(DigitalPin.P15, 0)
         動く左 = 0 - 左タイヤ
     }
-    pins.analogWritePin(AnalogPin.P13, 動く左)
-    pins.analogWritePin(AnalogPin.P16, 動く右)
+    P13_PWM = 1023 * (動く左 / 100)
+    if (P13_PWM > 1023) {
+        P13_PWM = 1023
+    }
+    P16_PWM = 1023 * (動く右 / 100)
+    if (P16_PWM > 1023) {
+        P16_PWM = 1023
+    }
+    pins.analogWritePin(AnalogPin.P13, P13_PWM)
+    pins.analogWritePin(AnalogPin.P16, P16_PWM)
 }
 basic.forever(function () {
     動く(900, 900)
